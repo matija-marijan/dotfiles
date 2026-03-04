57ef004cedcfd08367bde580f0c0b863bb88320b
Matija Marijan <matijamarijan99@gmail.com>
2026-03-04 16:43:19 +0100

    Update bufferline config

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,10 +1,20 @@
 return {
   "akinsho/bufferline.nvim",
   opts = function(_, opts)
+    -- use middle mouse click to close buffer
     opts.options.middle_mouse_command = function(n)
       Snacks.bufdelete(n)
     end
-    -- optionally disable right-click closing:
-    -- opts.options.right_mouse_command = false
+    -- disable right mouse click
+    opts.options.right_mouse_command = false
+
+    -- filter out opencode buffers
+    opts.options.custom_filter = function(buf_num, _)
+      local buf_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf_num), ":t")
+      -- Hide opencode panel from bufferline
+      if buf_name:match("^opencode") or buf_name:match("^OpenCode") then
+        return false
+      end
+    end
   end,
 }
