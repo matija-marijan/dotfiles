0e195d687097a032010ff7e68c7d954e0eb70efd
Matija Marijan <matijamarijan99@gmail.com>
2026-03-04 10:08:32 +0100

    Fix bufremove deprecation

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/bufferline.nvim",
   opts = function(_, opts)
     opts.options.middle_mouse_command = function(n)
-      LazyVim.ui.bufremove(n)
+      Snacks.bufdelete(n)
     end
     -- optionally disable right-click closing:
     -- opts.options.right_mouse_command = false
