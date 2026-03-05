c63d1458e3c7072d607700d7392f7e982df61ad4
Matija Marijan <matijamarijan99@gmail.com>
2026-03-05 09:32:37 +0100

    Fix lazygit pop up terminal size

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -2,8 +2,16 @@ return {
   "folke/snacks.nvim",
   opts = {
     styles = {
+      -- Style for regular terminal (Ctrl+/ toggle)
       terminal = {
-        height = 0.325, -- change to whatever fraction you want, e.g. 0.25
+        height = 0.325, -- Your desired small height
+        position = "bottom", -- Bottom split
+      },
+      -- Style for lazygit (independent sizing)
+      lazygit = {
+        height = 0.9, -- Larger height for lazygit
+        width = 0.9, -- Larger width for lazygit
+        position = "float", -- Floating window
       },
     },
   },
