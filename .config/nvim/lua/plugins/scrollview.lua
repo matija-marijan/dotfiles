3c65d5522e888dbd6a91f1fb3e2993e627bd49dc
Matija Marijan <matijamarijan99@gmail.com>
2026-05-17 11:21:25 +0200

    Add git signs to scrollbar

--- .config/nvim/lua/plugins/scrollview.lua
+++ .config/nvim/lua/plugins/scrollview.lua
@@ -5,11 +5,19 @@ return {
     require("scrollview").setup({
       excluded_filetypes = {
         "prompt", "TelescopePrompt", "noice", "notify", "lazy",
-        "NvimTree", "neo-tree", "blink-cmp-menu", "dropbar_menu"
+        "NvimTree", "neo-tree", "blink-cmp-menu", "dropbar_menu",
       },
-      signs_on_startup = { "diagnostics", "search" },
+      signs_on_startup = { "diagnostics", "search", "gitsigns" },
       current_only = false,
       winblend = 30,
+    })
+    require("scrollview.contrib.gitsigns").setup()
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        vim.api.nvim_exec_autocmds("User", { pattern = "GitSignsUpdate", modeline = false })
+      end,
+      desc = "Force scrollview gitsigns refresh on buffer enter",
     })
   end,
 }
